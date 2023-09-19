c7ffa45b10a93d8da30dee1ea9f5e2af91250017
dhananjay.singh <dhananjay.singh@e2enetworks.com>
2023-09-19 16:39:58 +0530

    Modified code to give prediction for more than one instances

--- codellama-container.py
+++ codellama-container.py
@@ -27,25 +27,33 @@ class MetaLLMA2Model(Model):
     def predict(self, payload: Dict, headers: Dict[str, str] = None) -> Dict:
         print("payload:", payload)
         inputs = payload["instances"]
-        source_text = inputs[0]["text"]
-
-        sequences = self.pipeline(source_text,
-                                  do_sample=True,
-                                  top_k=10,
-                                  top_p=0.95,
-                                  temperature=0.1,
-                                  num_return_sequences=1,
-                                  eos_token_id=self.tokenizer.eos_token_id,
-                                  max_length=200,
-                                  )
 
         results = []
-        for seq in sequences:
-            print(f"Result: {seq['generated_text']}")
-            results.append(seq['generated_text'])
+
+        for input in inputs:
+            source_text = input.get("text")
+            max_length = input.get("max_length", 100)
+            do_sample = input.get("do_sample", True)
+            top_k = input.get("top_k", 10)
+            top_p = input.get("top_p", 0.95)
+
+            sequences = self.pipeline(source_text,
+                                    do_sample=do_sample,
+                                    top_k=top_k,
+                                    top_p=top_p,
+                                    num_return_sequences=1,
+                                    eos_token_id=self.tokenizer.eos_token_id,
+                                    max_length=max_length,
+                                    )
+
+            result = []
+            for seq in sequences:
+                print(f"Result: {seq['generated_text']}")
+                result.append(seq['generated_text'])
+            results.append(result)
         
         return {"predictions": results}
 
 if __name__ == "__main__":
-    model = MetaLLMA2Model("codellama/CodeLlama-7b-hf")
+    model = MetaLLMA2Model("codellama")
     ModelServer().start([model])
